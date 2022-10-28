c85c983a29c83d0048a62938038d757c27a92202
https://github.com/monleak <hieu16112001@gmail.com>
2022-10-28 16:15:44 +0700

    Done

--- src/main/Main.java
+++ src/main/Main.java
@@ -16,7 +16,7 @@ public class Main {
     public static void main(String[] args) {
         Params.recordsNum = 1000;
         Problem prob = new Problem();
-        for(int testCase = 14;testCase<prob.testCase.size();testCase++){
+        for(int testCase = 0;testCase<prob.testCase.size();testCase++){
 
             for (int seed = 0;seed < Params.REPT;seed++){
                 System.out.println("=============== Test case " + (testCase+1) +" Seed "+ seed +" =================");
